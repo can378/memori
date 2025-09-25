acb0333051f969d9e97b6e4247960f16daefe752
can378 <yunji378mm@naver.com>
2025-09-25 11:07:39 +0900

    Docs:readme

--- app/README.md
+++ app/README.md
@@ -1,11 +1,7 @@
-db 연결 실험
-python example.py
-python tests/connect_database/mariadb_conn_test.py
 
-혹은
-memory/databse/manager/db_connection.py에서 주석빼고 하면됨
+# DB connection test
+memory/databse/manager/db_connection.py에서 주석빼고 단일 실행
 
-
-
+# RUN
 루트에서 실행
 uvicorn app.main:app --reload
--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI, Request
-from sqlalchemy.orm import Session
-from sqlalchemy import text
-from app.core.db_connection import get_db, test_connection
+from app.core.db_connection import test_connection
 from app.domains.memori_chat.chat_router import router as memori_chat_router
 from app.domains.memori_chat.session_router import router as memori_chat_session_router
 from app.core.context import set_session_id, reset_session_id 
 
 app = FastAPI()
 
-
+# default api====================================================
 @app.get("/")
 def root():
     return {"message": "Hello!"}
@@ -20,7 +18,7 @@ def health_check():
     return {"db_connection": "ok" if ok else "failed"}
 
 
-# 전달받은 cookie의 session_id를 ContextVar에 세팅
+# 전달받은 cookie의 session_id를 ContextVar에 세팅===================
 @app.middleware("http")
 async def session_cookie_ctx(request: Request, call_next):
     sid = request.cookies.get("session_id")
@@ -32,5 +30,6 @@ async def session_cookie_ctx(request: Request, call_next):
         reset_session_id(token)
 
 
+# add routers======================================================
 app.include_router(memori_chat_router)
 app.include_router(memori_chat_session_router)
